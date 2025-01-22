abb4f5e81c6471fe793dcd428de6490f9237eea9
angel andres miranda castillo <mirandaangel83@gmail.com>
2025-01-21 19:52:30 -0500

    Added error handling for topico, respuesta y usuario

--- src/main/java/com/OracleOne/ForoHub/infrastructure/exceptionHandler/ControllerAdvisor.java
+++ src/main/java/com/OracleOne/ForoHub/infrastructure/exceptionHandler/ControllerAdvisor.java
@@ -1,6 +1,9 @@
 package com.OracleOne.ForoHub.infrastructure.exceptionHandler;
 
+import com.OracleOne.ForoHub.domain.exceptions.RespuestaNotFoundException;
 import com.OracleOne.ForoHub.domain.exceptions.TopicoAlreadyExistsException;
+import com.OracleOne.ForoHub.domain.exceptions.TopicoNotFoundException;
+import com.OracleOne.ForoHub.domain.exceptions.UsuarioAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +16,21 @@ public class ControllerAdvisor {
 
     @ExceptionHandler(TopicoAlreadyExistsException.class)
     public ResponseEntity<Map<String,String>> handleTopicoAlreadyExistsException(TopicoAlreadyExistsException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("message",e.getMessage()));
+    }
+
+    @ExceptionHandler(TopicoNotFoundException.class)
+    public ResponseEntity<Map<String,String>> handleTopicoNotFoundException(TopicoNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message",e.getMessage()));
+    }
+
+    @ExceptionHandler(RespuestaNotFoundException.class)
+    public ResponseEntity<Map<String,String>> handleRespuestaNotFoundException(RespuestaNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message",e.getMessage()));
+    }
+
+    @ExceptionHandler(UsuarioAlreadyExistsException.class)
+    public ResponseEntity<Map<String,String>> handleUsuarioAlreadyExistsException(UsuarioAlreadyExistsException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("message",e.getMessage()));
     }
 }
